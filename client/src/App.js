4428602e2e83f8a34d4af2dea0fe1c455cc04532
arushpsingh <arushp59@gmail.com>
2022-05-25 14:52:57 +0530

    database updated

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return(
+    <>
+      <h2>ok</h2>
+    </>
+  )
+}
+
+export default App;
+
---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postsRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use('/posts', postsRoutes);
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://arushpsingh:mongo123@cluster0.dz7nc.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, ()=>console.log(`Server running on PORT: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
